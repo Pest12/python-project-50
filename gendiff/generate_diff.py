c1b18aeb3f40707d70647ae8c37294b2c0fa937b
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-08-22 19:51:12 +0500

    complete 3 step

---
+++ gendiff/generate_diff.py
@@ -0,0 +1,28 @@
+import json
+
+
+def to_str(value):
+    if isinstance(value, str):
+        return value
+    elif isinstance(value, bool):
+        return str(value).lower()
+    elif isinstance(value, int):
+        return value
+
+
+def generate_diff(file_path1, file_path2):
+    diff = []
+    file1 = json.load(open(file_path1))
+    file2 = json.load(open(file_path2))
+    for key in sorted(file1 | file2):
+        if key in file1 and key in file2:
+            if file1[key] == file2[key]:
+                diff.append(f'    {key}: {to_str(file1[key])}')
+            else:
+                diff.append(f'  - {key}: {to_str(file1[key])}\n  + {key}: {to_str(file2[key])}')
+        elif key in file1 and key not in file2:
+            diff.append(f'  - {key}: {to_str(file1[key])}')
+        elif key not in file1 and key in file2:
+            diff.append(f'  + {key}: {to_str(file2[key])}')
+    template = '{\n' + '\n'.join(diff) + '\n}'
+    return template
--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from gendiff import generate_diff
 
 
 def main():
@@ -8,7 +9,8 @@ def main():
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
     args = parser.parse_args()
-
+    diff = generate_diff('file1.json', 'file2.json')
+    print(diff)
 
 if __name__ == '__main__':
     main()

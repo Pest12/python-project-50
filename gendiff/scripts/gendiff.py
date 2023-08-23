8459e231f9b9e5771a601ab881060c225b078636
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-08-23 19:08:29 +0500

    flake8

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,11 +4,11 @@ from gendiff import generate_diff
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
+    parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')  # noqa: E501
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('-f', '--format', help='set format of output')
-    args = parser.parse_args()
+    args = parser.parse_args()  # noqa: F841
     diff = generate_diff('file1.json', 'file2.json')
     print(diff)
 

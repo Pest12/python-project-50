cc2a62899147c037b7b3ece66a142f9d13ebb762
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-09-15 16:53:46 +0500

    update generate

--- gendiff/generate.py
+++ gendiff/generate.py
@@ -7,17 +7,21 @@ def build_diff(old, new):
     for key in keys:
         if key not in new:
             res[key] = {'type': 'removed', 'value': old[key]}
-        elif key not in old:
+            continue
+        if key not in old:
             res[key] = {'type': 'added', 'value': new[key]}
-        elif isinstance(old[key], dict) and isinstance(new[key], dict):
+            continue
+        if isinstance(old[key], dict) and isinstance(new[key], dict):
             res[key] = {
                 'type': 'nested_dict', 'value': build_diff(old[key], new[key])
             }
-        elif old[key] != new[key]:
+            continue
+        if old[key] != new[key]:
             res[key] = {
                 'type': 'updated', 'old_value': old[key],
                 'new_value': new[key]
             }
-        elif old[key] == new[key]:
+            continue
+        if old[key] == new[key]:
             res[key] = {'type': 'same', 'value': old[key]}
     return OrderedDict(sorted(res.items()))

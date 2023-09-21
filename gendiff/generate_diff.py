e6677d6100de7d4e62a91ff4756db7095c046608
Konstantin Lugovskikh <kostya.lugovskikh@mail.ru>
2023-09-21 18:18:43 +0500

    update generate_diff

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -8,4 +8,3 @@ def generate_diff(file_path1, file_path2, formater='stylish'):
     content2 = get_content(file_path2)
     dicts_diff = build_diff(content1, content2)
     return get_formater(dicts_diff, formater)
-
